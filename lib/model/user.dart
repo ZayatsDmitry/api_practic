dd6751fbf08e895d98a4f118fd170a8baf8a63be
ZayatsDmitry <zayatsdmitry93@gmail.com>
2023-08-24 22:23:54 +0300

    second day

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,3 +1,8 @@
+import 'package:riverpood_practic/model/user_dob.dart';
+import 'package:riverpood_practic/model/user_location.dart';
+import 'package:riverpood_practic/model/user_name.dart';
+import 'package:riverpood_practic/model/user_picture.dart';
+
 class User {
   final String gender;
   final String email;
@@ -5,22 +10,41 @@ class User {
   final String cell;
   final String nat;
   final UserName name;
-  User(
-      {required this.gender,
-      required this.email,
-      required this.phone,
-      required this.cell,
-      required this.nat,
-      required this.name});
-}
+  final UserDob dob;
+  final UserLocation location;
+  final UserPicture picture;
 
-class UserName {
-  final String title;
-  final String first;
-  final String last;
-  UserName({
-    required this.title,
-    required this.first,
-    required this.last,
+  User({
+    required this.gender,
+    required this.email,
+    required this.phone,
+    required this.cell,
+    required this.nat,
+    required this.name,
+    required this.dob,
+    required this.location,
+    required this.picture,
   });
+
+  factory User.fromMap(Map<String, dynamic> e) {
+    final name = UserName.fromMap(e["name"]);
+    final dob = UserDob.fromMap(e["dob"]);
+    final location = UserLocation.fromMap(e["location"]);
+    final picture = UserPicture.fromMap(e["picture"]);
+    return User(
+      cell: e["cell"],
+      email: e["email"],
+      phone: e["phone"],
+      gender: e["gender"],
+      nat: e["nat"],
+      name: name,
+      dob: dob,
+      location: location,
+      picture: picture,
+    );
+  }
+
+  String get fullName {
+    return "${name.title} ${name.first} ${name.last}";
+  }
 }
